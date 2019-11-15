91013f9fb3b9be594c17e86cd96f34a2a358eb59
Harrison Banh <harrisonbanh+gitlab@gmail.com>
2019-11-15 12:28:44 -0500

    [Pushbutton Timing Tester Verified]
    Refactored the program to make use of the RTOS. As of right now, we can
    successfully flash an led and measure the user's reaction time to it by using a
    pushbutton.

--- main.cpp
+++ main.cpp
@@ -1,33 +1,53 @@
 #include "mbed.h"
+#include "rtos.h"
 #include "PinDetect.h"
 
 
-Serial pc(USBTX, USBRX);
+RawSerial pc(USBTX, USBRX);
 DigitalOut myled(LED1);
-PinDetect pb(p19, PullDown);
+PinDetect pb(p26, PullDown);
 Timer t;
+const int TEN_SEC_DEL = 10000;
+volatile float rxn_time = 0;
+volatile int input = 0;
 
-void flash(DigitalOut* led) {
-    *led = 1;
-    t.start();
-    float weight = rand() / (float) RAND_MAX;
-    wait(3 * weight);
-    *led = 0;
+void flash(void const *args) {
+    while(1){
+        myled = 1;
+        t.start();
+        float weight = rand() / (float) RAND_MAX;
+        Thread::wait(1000);
+        myled = 0;
+        Thread::wait( (int) (TEN_SEC_DEL * weight));
+    }
+}
+
+void button_ready(void) {
+    input = 1;
+    rxn_time = t.read();
+    t.stop();
+    t.reset();
 }
 
 int main() {
 
     pc.printf("System starting in ...");
-    for(int i = 1; i <= 5; i++) {  
+    for(int i = 1; i <= 5; i++) {
         pc.printf("%d...", i);
+        wait(1);
     }
-    pc.printf("Go!");
+    pc.printf("Go!\n\r");
+    pb.attach_deasserted(&button_ready);
+    pb.setSampleFrequency();
+
+
+    Thread thread1(flash);
 
     while(1) {
-        flash(&myled);
-        if (pb == 1) {
-            float time = t.read();
-            pc.printf("Reaction Time: %f ms", time);
+        if (input == 1) {
+            input = 0;
+            pc.printf("Reaction Time: %f s\n\r", rxn_time);
         }
+        Thread::wait(100);
     }
 }
